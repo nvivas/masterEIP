40807dc625d81da3db716a54264caf0a66b97186
delpa <moeshe@gmail.com>
2022-06-20 18:41:42 +0200

    avanzando

--- proyecto/js/index.js
+++ proyecto/js/index.js
@@ -1,21 +1,12 @@
-function navegador() {
-  let navegar = "";
-  navegar += "<nav>";
-  navegar += "<menu>";
-  navegar += "<li><a href='index.html' class='active'>Inicio</a></li>";
-  //  <li>
-  //    <a href="juegosMesa.html">Juegos de mesa</a>
-  //  </li>
-  //  <li>
-  //    <a href="rol.html">Rol ESDLA</a>
-  //  </li>
-  //  <li>
-  //    <a href="contacto.html">Contacto</a>
-  //  </li>
-  navegar += "</menu>";
-  navegar += "</nav>";
+function mueveReloj() {
+  momentoActual = new Date();
+  hora = momentoActual.getHours();
+  minuto = momentoActual.getMinutes();
+  segundo = momentoActual.getSeconds();
+
+  horaImprimible = hora + " : " + minuto + " : " + segundo;
+
+  document.form_reloj.reloj.value = horaImprimible;
+
+  setTimeout("mueveReloj()", 1000);
 }
-
-const menu = document.querySelector("#navegador");
-
-menu.innerHTML = "<h1>Tema 1</h1>";
