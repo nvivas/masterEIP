112536073899129a7f1aa7a7129029a8d7878da2
delpa <moeshe@gmail.com>
2022-06-22 17:04:49 +0200

    html js y css

--- proyecto/js/index.js
+++ proyecto/js/index.js
@@ -10,3 +10,34 @@ function mueveReloj() {
 
   setTimeout("mueveReloj()", 1000);
 }
+
+// prueba para el menú
+const toggleButton = document.getElementById("button-menu");
+const navWrapper = document.getElementById("nav");
+
+toggleButton.addEventListener("click", () => {
+  toggleButton.classList.toggle("close");
+  navWrapper.classList.toggle("show");
+});
+
+navWrapper.addEventListener("click", (e) => {
+  if (e.target.id === "nav") {
+    navWrapper.classList.remove("show");
+    toggleButton.classList.remove("close");
+  }
+});
+// fin prueba menú
+
+const animacion = anime({
+  targets: ".line-drawing-demo .lines path",
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: "easeInOutSine",
+  duration: 1500,
+  delay: function (el, i) {
+    return i * 250;
+  },
+  direction: "alternate",
+  loop: true,
+});
+
+document.querySelector(".juegosTitulo").onclick = animacion.restart;
