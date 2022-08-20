7bb41d09e1d91b7e1d009b75abd7797dd91ac8f5
nvivas <moeshe@gmail.com>
2022-08-20 09:34:59 +0200

    leccion 5 eventos (#49)

--- Angular/proyectoAngular/src/app/app.module.ts
+++ Angular/proyectoAngular/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,7 +21,7 @@ import { DirectivaComponent } from './elementos/directiva/directiva.component';
     EstiloPropioDirective,
     DirectivaComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
